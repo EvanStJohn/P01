111c8df4e3ec2ad97474a601cba5e4b8ced2dd5f
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2019-10-17 10:50:42 -0700

    add project skeleton

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class StartWars {
+private:
+    struct warrior{
+        int id;
+        string name;
+        int timeCounter;
+    };
+public:
+    void createClone();
+    void lifespan();
+};
+
+int main() {
+
+    return 0;
+}
