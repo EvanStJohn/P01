a9916a90aa73b2f9a7da6eacb8ca167f4b92e10a
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2019-10-21 10:52:09 -0700

    add input

--- main.cpp
+++ main.cpp
@@ -9,12 +9,33 @@ private:
         string name;
         int timeCounter;
     };
+    vector<warrior> clones;
 public:
     void createClone();
     void lifespan();
+    void input();
 };
 
+void StartWars::createClone()
+{
+    int input;
+    cout << "how many clones should there be" << endl;
+    cin >> input;
+
+    clones.reserve(input);
+}
+
+void StartWars::input()
+{
+    cout << "what is the id" << endl;
+
+    cout << "what is the name" << endl;
+
+    cout << "what is their " << endl;
+}
+
 int main() {
+    StartWars start;
 
     return 0;
 }
